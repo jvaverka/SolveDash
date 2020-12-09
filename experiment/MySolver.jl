8c644b946460a774d0b537dab9a717b884d5a5fa
J Vaverka <jvaverka@protonmail.com>
2020-12-09 17:38:15 -0500

    unit crazy

--- experiment/MySolver.jl
+++ experiment/MySolver.jl
@@ -5,7 +5,7 @@ using MyFields
 using Parameters
 
 
-export solver_base, find_acceleration, convert_field_value!, hasunit, hasvalue
+export solver_base, find_acceleration, convert_field_value!, convert_field_values!, revert_units!, hasunit, hasvalue
 
 function solver_base(ic::MyConditions.BaseConditions)
     
@@ -14,21 +14,23 @@ function solver_base(ic::MyConditions.BaseConditions)
     solns = Dict()
 
     if a.find
-        
-        """ try solving v = v₀ + at for a """
         if hasvalue([v, v₀, t]) && hasunit([v, v₀, t])
-            convert_field_value!(v, a)
-            convert_field_value!(v₀, a)
-            convert_field_value!(t, a)
-
+            convert_field_values!([v, v₀, t], a)
             solns["acceleration"] = find_acceleration(v, v₀, t)
         end
-
-        """ try solving  """
-
+        if hasvalue([x₀, x, v₀, t]) && hasunit([x₀, x, v₀, t])
+            convert_field_values!([x₀, x, v₀, t], a)
+            solns["acceleration"] = find_acceleration(x, x₀, v₀, t)
+        end
+        if hasvalue([v, v₀, x, x₀]) && hasunit([v, v₀, x, x₀])
+            convert_field_values!([v, v₀, x, x₀], a)
+            solns["acceleration"] = find_acceleration(v, v₀, x, x₀)
+        end
     end
     if t.find
-        # solve for time
+        if hasvalue([v, v₀, a]) && hasunit([v, v₀, a])
+            convert_field_values!([v, v₀, a], t)
+        end
     end
     if v̄.find
         # solve for average velocity
@@ -47,32 +49,81 @@ function solver_base(ic::MyConditions.BaseConditions)
     end
 end
 
+" vₓ = vₓ₀ + aₓt "
 function find_acceleration(
-    v::MyFields.VelocityField,
-    v₀::MyFields.VelocityField,
-    t::MyFields.TimeField,
+    v::VelocityField,
+    v₀::VelocityField,
+    t::TimeField,
     )
 
     (v.val - v₀.val) / t.val
 end
 
-""" Convert PositionField units to match AccelerationField """
-function convert_field_value!(p::MyFields.PositionField, a::MyFields.AccelerationField)
-    p.val = p.val |> a.tu
+" x = x₀ + vₓ₀t + (1//2)aₓt² "
+function find_acceleration(
+    x::PositionField,
+    x₀::PositionField,
+    v₀::VelocityField,
+    t::TimeField,
+    )
+
+    (2*(x.val - x₀.val - v₀.val*t.val))/t.val^2
 end
 
-""" Convert TimeField units to match AccelerationField """
-function convert_field_value!(t::MyFields.TimeField, a::MyFields.AccelerationField)
-    t.val = t.val |> a.tu
+" vₓ² = vₓ₀² + 2aₓ(x - x₀) "
+function find_acceleration(
+    v::VelocityField,
+    v₀::VelocityField,
+    x::PositionField,
+    x₀::PositionField,
+)
+    (v.val^2 - v₀.val^2) / (2 * (x.val - x₀.val))
 end
 
-""" Convert VelocityField units to match AccelerationField """
-function convert_field_value!(v::MyFields.VelocityField, a::MyFields.AccelerationField)
-    v.val = v.val |> a.lu/a.tu
+" vₓ = vₓ₀ + aₓt "
+function find_time(v::VelocityField, v₀::VelocityField, a::AccelerationField)
+    (v.val - v₀.val) / a.val
 end
 
-""" Ensure all fields in a given list have units """
-function hasunit(fields::Vector{<:MyFields.AbstractField})
+" Convert field values for all fields in a given vector "
+function convert_field_values!(fields::Vector{<:AbstractField}, target::T) where T<:AbstractField
+    for field ∈ fields
+        convert_field_value!(field, target)
+    end
+end
+
+" Convert PositionField units to match AccelerationField "
+function convert_field_value!(p::PositionField, a::AccelerationField)
+    setfield!(p, :val, p.val |> a.lu)
+end
+
+" Convert TimeField units to match AccelerationField "
+function convert_field_value!(t::TimeField, a::AccelerationField)
+    setfield!(t, :val, t.val |> a.tu)
+end
+
+" Convert VelocityField units to match AccelerationField "
+function convert_field_value!(v::VelocityField, a::AccelerationField)
+    setfield!(v, :val, v.val |> a.lu/a.tu)
+end
+
+" Convert VelocityField units to match TimeField "
+function convert_field_value!(v::VelocityField, t::TimeField)
+    setfield!(v, :val, v.val |> v.lu/t.unit)
+end
+
+" Convert AccelerationField units to match TimeField "
+function convert_field_value!(a::AccelerationField, t::TimeField)
+    setfield!(a, :val, a.val |> a.lu/t.unit^2)
+end
+
+" Revert field value back to original units "
+function revert_units!(field::T) where T<:AbstractField
+    setfield!(field, :val, field.val |> field.unit)
+end
+
+" Ensure all fields in a given list have units "
+function hasunit(fields::Vector{<:AbstractField})
     for f ∈ fields
         if isnothing(f.unit)
             return false
@@ -81,8 +132,8 @@ function hasunit(fields::Vector{<:MyFields.AbstractField})
     return true
 end
 
-""" Ensure all fields in a given list have values """
-function hasvalue(fields::Vector{<:MyFields.AbstractField})
+" Ensure all fields in a given list have values "
+function hasvalue(fields::Vector{<:AbstractField})
     for f ∈ fields
         if isnothing(f.val)
             return false
--- experiment/MySolverTest.jl
+++ experiment/MySolverTest.jl
@@ -8,11 +8,11 @@ import Unitful:
     mm, cm, m, km, inch, ft, yd, mi,
     ns, μs, ms, s, minute, hr, wk, yr,
     ustrip, @u_str
-    
+
 
 @testset "utility functions" begin
     b = BaseConditions(
-        PositionField(0, m, false),
+        PositionField(0m, m, false),
         PositionField(nothing, nothing, false),
         VelocityField(0m/s, m/s, m, s, false),
         VelocityField(423//25*km/minute, km/minute, km, minute, false),
@@ -20,6 +20,15 @@ import Unitful:
         TimeField(5s, s, false),
         AccelerationField(nothing, m/s^2, m, s, true)
     )
+
+    pf1 = PositionField(1m, m, false)
+    pf2 = PositionField(10km, km, false)
+    vf1 = VelocityField(2km/hr, km/hr, km, hr, false)
+    vf2 = VelocityField(400*km/minute, km/minute, km, minute, false)
+    vf3 = VelocityField(55yd/μs, yd/μs, yd, μs, false)
+    tf1 = TimeField(5ns, ns, false)
+    af1 = AccelerationField(12ft/s^2, ft/s^2, ft, s, true)
+
     @unpack x₀, x, v₀, v, v̄, t, a = b
     @testset "hasunit" begin
         @test hasunit([x, v̄]) == false
@@ -29,11 +38,23 @@ import Unitful:
         @test hasvalue([x, v̄]) == false
         @test hasvalue([x₀, v₀, v, t]) == true
     end
-    @testset "convert fields" begin
-        convert_field_value!(t, a)
+    @testset "convert single field value" begin
+        @test convert_field_value!(t, a) == 5s
+        @test convert_field_value!(v, a) == 282m/s
+        @test convert_field_value!(x₀, a) == 0km
+        @test convert_field_value!(vf1, tf1) == 2km/ns
+        @test convert_field_value!(af1, tf1) == (3//250000000000000000)*ft/ns^-2
+    end
+    @testset "revert units" begin
+        @test revert_units!(t) == 5s
+        @test revert_units!(v) == 423//25*km/minute
+        @test revert_units!(x₀) == 0m
+    end
+    @testset "convert multiple field values" begin
+        convert_field_values!([t, v, x₀], a)
         @test t.val == 5s
-        convert_field_value!(v, a)
         @test v.val == 282m/s
+        @test x₀.val == 0km
     end
     @testset "find acceleration" begin
         @test find_acceleration(v, v₀, t) == (282//5)*m/s^2
